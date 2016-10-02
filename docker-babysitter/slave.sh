9aa56165dfc5669d64f59589060467dfb0ba1a25
Jan.Helge.Salvesen <Jan.Helge.Salvesen@gmail.com>
2016-10-02 11:21:24 +0200

    Better formatting and removed unused parameters/variables

--- docker-babysitter/slave.sh
+++ docker-babysitter/slave.sh
@@ -2,32 +2,30 @@
 Documetation(){ : <<-! | tr -d '#' # >README.md
 # DOC: This script tracks the $YmlDir directory for yml-files
 # and starts/stops docker containers accordingly. If an yml file
-# is removed, and the script refreshes, the corresponding docker
-# container is terminated. 
+# is removed, and the script is signalled (eg. kill -HUP <pid>), the 
+# corresponding docker container(s) is/are terminated. 
 #
 # This script responds to two signals
 #  * INT (aka ctrl-c)  Refreshes 
 #        the configuration and adopts to changes
 #  * TERM cleanup and terminate
 #
-# This script is intended to run as a slave script of
-# a daemon procsss (see docker-babysitter-daemon.sh)
+# This script is useful on its own, but is also capable
+# to run as a slave script of a linux lsb-daemon process such as 
+# docker-babysitter-daemon.sh
+# 
 !
 }; # Documetation; exit
 
 verboseLevel=1 # 0==Only_ERROR, 1==INFO_AndBelow, 2==DEBUG_AndBelow
 YmlDir=/z/deploy/yml
-DockerStopGracetime="--time=1"
-# PrivateTemporaryFile=/tmp/slave-tmp-$$.tmp
-Adopted=""
-Echo(){ echo $(date "+%Y-%m-%d %H:%M.%S") $*; }
-ERROR(){ 	if [ "$verboseLevel" -ge 0 ];then Echo ERROR 	$*;fi; }
-INFO(){ 	if [ "$verboseLevel" -ge 1 ];then Echo INFO 	$*;fi; }
-DEBUG(){ 	if [ "$verboseLevel" -ge 2 ];then Echo DEBUG 	$*;fi; }
 Doit(){
 	DieIfNotDockerd
 	# BrutalStopAndRemoveContainers; exit
-	# MakeYamls
+	MainLoop
+	
+}
+MainLoop(){
 	on_ctrl_c() {
 		DEBUG Will terminate on CTRL-C
 		Adopt /dev/null # Adopt as if no yamls was active
@@ -49,11 +47,11 @@ Doit(){
 Reverse(){ echo $*|awk '{for (i=NF; i>0; i--) printf "%s ",$(i);}'; }
 Memorize(){ eval mem_$1=$(cat $2|gzip|base64 -w0); DEBUG memorized $1; }
 Docker(){
-	echo
-	echo '#About to run dockercommand:'
-	echo '##################'
-	echo docker $*
-	echo '##################'
+	local _rc
+	echo '# About to run following docker command'
+	echo docker $*; docker $*; _rc=$?
+	echo '###############################' RC=$_rc
+	return $_rc
 }
 
 LoadIfFileAndNotLoaded(){
@@ -68,6 +66,7 @@ LoadIfFileAndNotLoaded(){
 		DEBUG $img Was already loaded
 	fi
 }
+
 Adopt(){
 	# d1=normally $YmlDir, exept when terminating
 	local news=$(Actual $1) _x _pod
@@ -88,9 +87,11 @@ Adopt(){
 		DEBUG No change to Adopt
 	fi
 }
+
 AnotinB(){
 # Enries is a colon-separated list as in /a/file.yml:/a/file2.yml 
 # This function returns enries in $1 that is not in $2. 
+# Eg: AnotinB a:b:c a:c returns b
 	local _a _b	
 	for _a in $(echo $1|tr : " "); do
 		for _b in $(echo $2|tr : " "); do
@@ -99,7 +100,9 @@ AnotinB(){
 		echo $_a
 	done
 }
-BaseFromYmlFileName(){ basename $1|sed 's/\.yml$//g';}
+
+BaseFromYmlFileName(){ basename $1|sed 's/\.yml$//';}
+
 Actual(){ 
 	# Will normally have arg1=$YmlDir, but /dev/null when terminating
 	local dir=$1 y accumul colon;
@@ -110,6 +113,7 @@ Actual(){
 	done
 	if [ "$accumul" != "" ];then echo $accumul; fi
 }
+
 DieIfNotDockerd(){
 	local retry=3
 	while [ $retry -gt 0 ];do
@@ -145,11 +149,7 @@ StartContainerFromYml(){
 		_dockerimage=$(eval echo \$${_x}dockerimage)
 		# DEBUG $_serv dockerimage: $_dockerimage Flags=${_runflags} Args: ${_runargs}
 		LoadIfFileAndNotLoaded $_dockerimage
-		# rm -f $PrivateTemporaryFile
-		# docker run --cidfile $PrivateTemporaryFile ${_runflags} $_serv ${_runargs}
-		# _x=$(cat $PrivateTemporaryFile; rm -f $PrivateTemporaryFile)
 		docker run ${_runflags} $_serv ${_runargs}
-		DEBUG Started $_serv as cid $_x
 	done
 }
 
@@ -158,7 +158,7 @@ UseMemorizedConfigAndTerminate(){
 	_v=$(eval echo \$mem_$_pod)
 	eval $(echo $_v|base64 -d|zcat|ParseYaml ${_pod}_)
 	_services=$(eval echo \$${_pod}_services)
-	DEBUG Bring down services in sequence $(Reverse $_services)
+	INFO Brings down services in sequence $(Reverse $_services)
 	_v=$(docker stop $DockerStopGracetime $(Reverse $_services)); DEBUG Stopped $_v
 	_v=$(docker rm $(Reverse $_services)); DEBUG cleaned up $_v
 }
@@ -173,6 +173,7 @@ BrutalStopAndRemoveContainers(){
 		DEBUG No container to remove.
 	fi
 }
+
 ParseYaml(){
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo "\034")
    sed -ne "s|^\($s\):|\1|" \
@@ -186,32 +187,11 @@ ParseYaml(){
       }
    }'
 }
-UbuntuYaml(){
-	# Fasit: docker run -itd ubuntu bash
-	cat <<-!
-		services: ubuntu
-		  ubuntu:
-		    runflags: -itd --name ubuntu 
-		    runargs: bash
-	!
-}
-FotoregYaml(){
-	local e=""
-	e="$e -e POSTGRES_PASSWORD=pass"
-	e="$e -e POSTGRES_USER=jhs009"
-	e="$e -e POSTGRES_DB=fangefoto"
-	cat <<-!
-		services: mypg fotoreg
-		  mypg:
-		    dockerimage: /z/docker/mypg.dockerimage
-		    runflags: $e --name mypg -d
-		  fotoreg: 
-		    dockerimage: /z/docker/fotoreg.dockerimage
-		    runflags: -d -p 9191:9191 --link mypg:mypg --net=host
-	!
-}
-MakeYamls(){
-	FotoregYaml 	> $YmlDir/fotoregws.yml
-	UbuntuYaml 	> $YmlDir/myubuntu.yml
-}
+DockerStopGracetime="--time=1"
+# PrivateTemporaryFile=/tmp/slave-tmp-$$.tmp
+Adopted=""
+Echo(){ echo $(date "+%Y-%m-%d %H:%M.%S") $*; }
+ERROR(){ 	if [ "$verboseLevel" -ge 0 ];then Echo ERROR 	$*;fi; }
+INFO(){ 	if [ "$verboseLevel" -ge 1 ];then Echo INFO 	$*;fi; }
+DEBUG(){ 	if [ "$verboseLevel" -ge 2 ];then Echo DEBUG 	$*;fi; }
 Doit $*
